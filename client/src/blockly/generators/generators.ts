c1c4746411d01f1d5bd8542e4ca67c69decf1272
samuelbarrett <samuelb.home@gmail.com>
2025-12-10 17:31:17 -0600

    update file structure

---
+++ client/src/blockly/generators/generators.ts
@@ -0,0 +1,73 @@
+/**
+ * @license
+ * Copyright 2023 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+// some code in this file is originally from Google and modified for the purposes of this project
+
+import * as Blockly from 'blockly/core';
+
+// Export all the code generators for our custom blocks,
+// but don't register them with Blockly yet.
+// This file has no side effects!
+export const forBlock = Object.create(null);
+
+// placeholder copied from block factory
+forBlock['robot_dialog'] = function(
+  block: Blockly.Block,
+  generator: Blockly.CodeGenerator, 
+) {
+  console.log("generating code for robot dialog block");
+  
+}
+
+// placeholder copied from block factory
+forBlock['listen_keyword_block'] = function(
+  block: Blockly.Block,
+  generator: Blockly.CodeGenerator,
+) {
+  console.log("generating code for listen keyword block");
+}
+
+forBlock['listen_block'] = function(
+  block: Blockly.Block,
+  generator: Blockly.CodeGenerator,
+) {
+  console.log("generating code for listen block");
+}
+
+forBlock['say_block'] = function(
+  block: Blockly.Block,
+  generator: Blockly.CodeGenerator,
+) {
+  console.log("generating code for say block");
+}
+
+// ------------- Style blocks -------------
+forBlock['style_happy_block'] = function(
+  block: Blockly.Block,
+  generator: Blockly.CodeGenerator,
+) {
+  console.log("generating code for happy style");
+}
+
+forBlock['style_sad_block'] = function(
+  block: Blockly.Block,
+  generator: Blockly.CodeGenerator,
+) {
+  console.log("generating code for sad style");
+}
+
+forBlock['style_excited_block'] = function(
+  block: Blockly.Block,
+  generator: Blockly.CodeGenerator,
+) {
+  console.log("generating code for excited style");
+}
+
+forBlock['style_bored_block'] = function(
+  block: Blockly.Block,
+  generator: Blockly.CodeGenerator,
+) {
+  console.log("generating code for bored style");
+}
--- client/src/blockly/toolbox.ts
+++ client/src/blockly/toolbox.ts
@@ -4,14 +4,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-/*
-This toolbox contains nearly every single built-in block that Blockly offers,
-in addition to the custom block 'add_text' this sample app adds.
-You probably don't need every single block, and should consider either rewriting
-your toolbox from scratch, or carefully choosing whether you need each block
-listed here.
-*/
-
 export const toolbox = {
     kind: 'categoryToolbox',
     contents: [
