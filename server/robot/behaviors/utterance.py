6d4925bb8991ce9a597e45505738ba87328ca12a
samuelbarrett <samuelb.home@gmail.com>
2025-12-15 12:54:23 -0600

    utterance behavior generator stub

---
+++ server/robot/behaviors/utterance.py
@@ -0,0 +1,12 @@
+import asyncio
+import random
+from robot.behaviors.RobotEvent import RobotEvent
+
+# Example behavior coroutines
+async def utterance_behavior(event_queue):
+    try:
+        while True:
+            await event_queue.put(RobotEvent(type='utterance', params={}))
+            await asyncio.sleep(random.uniform(1, 3))
+    except asyncio.CancelledError:
+        pass
