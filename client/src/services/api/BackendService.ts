92ed61d0daf69f5d1ea41b0196abdafe628c85a7
samuelbarrett <samuelb.home@gmail.com>
2026-01-03 20:34:46 -0600

    implement full API calls with parameters

--- client/src/services/api/BackendService.ts
+++ client/src/services/api/BackendService.ts
@@ -1,5 +1,6 @@
 // handles calls to the backend API
 
+import { StyleSchema } from '../../models/style.ts';
 import type { paths } from './openapi-types.ts';
 import createClient from 'openapi-fetch';
 
@@ -11,34 +12,45 @@ type listenSilenceResponse = paths['/command/listenSilence']['post']['responses'
 type listenKeywordRequestBody = paths['/command/listenKeyword']['post']['requestBody']['content']['application/json'];
 type listenKeywordResponse = paths['/command/listenKeyword']['post']['responses']['200']['content']['application/json'];
 
-const client = createClient<paths>({
+const backend = createClient<paths>({
   baseUrl: process.env.BASE_SERVER_URL,
 });
 
 export const backendService = {
   getStatus: async (): Promise<StatusResponse> => {
-    const {data, error} = await client.GET('/status');
+    const {data, error} = await backend.GET('/status');
     if (error || !data) {
       console.error('Error fetching status from backend:', error);
     }
     return data;
   },
-  speak: async (body: SpeakRequestBody): Promise<SpeakResponse> => {
-    const {data, error} = await client.POST('/command/speak', {body});
+  speak: async (
+    speech: string,
+    options?: StyleSchema
+  ): Promise<SpeakResponse> => {
+    const body: SpeakRequestBody = { speech };
+    const {data, error} = await backend.POST('/command/speak', {body});
     if (error || !data) {
       console.error('Error sending speak command to backend:', error);
     }
     return data;
   },
-  listenSilence: async (body: listenSilenceRequestBody): Promise<listenSilenceResponse> => {
-    const {data, error} = await client.POST('/command/listenSilence', {body});
+  listenSilence: async (
+    options?: StyleSchema
+  ): Promise<listenSilenceResponse> => {
+    const body: listenSilenceRequestBody = { options };
+    const {data, error} = await backend.POST('/command/listenSilence', {body});
     if (error || !data) {
       console.error('Error sending listenSilence command to backend:', error);
     }
     return data;
   },
-  listenKeyword: async (body: listenKeywordRequestBody): Promise<listenKeywordResponse> => {
-    const {data, error} = await client.POST('/command/listenKeyword', {body});
+  listenKeyword: async (
+    keyword: string,
+    options?: StyleSchema
+  ): Promise<listenKeywordResponse> => {
+    const body: listenKeywordRequestBody = { keyword, options };
+    const {data, error} = await backend.POST('/command/listenKeyword', {body});
     if (error || !data) {
       console.error('Error sending listenKeyword command to backend:', error);
     }
